971d2978b5582fe05fc7555e3aca94eb926ee41b
Jesper Persson <jespert.persson@gmail.com>
2013-11-05 12:19:59 +0100

    Initial Commit - Add file

--- projectanalysis.py
+++ projectanalysis.py
@@ -5,5 +5,11 @@ for root, dirs, files in os.walk("./"):
     for file in files:
         fileName, fileExtension = os.path.splitext(file)
         if fileExtension in VALID_EXSTENSIONS:
-             print (fileName, fileExtension)
+             print ("Path:", root, "File:", file, "Length:", file_length(file))
 
+
+def file_length(file_path):
+    with open(file_path) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
