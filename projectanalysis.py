7e5f6c88d96a8e22b3cdb3d5d3c4b8706485fcdd
Jesper Persson <jespert.persson@gmail.com>
2013-11-05 12:54:45 +0100

    Make program count non-empty rows

--- projectanalysis.py
+++ projectanalysis.py
@@ -1,15 +1,16 @@
 import os
 VALID_EXSTENSIONS = [".h", ".hpp", ".c", ".cpp", ".py", ".java", ".rb", ".html", ".css", ".erb", ".m"]
 
+def file_length(file_path):
+    with open(file_path) as f:
+        rowCount = 0
+        for i, l in enumerate(f):
+            if len(l) > 1: rowCount = rowCount + 1
+    return rowCount
+
+# Main program #
 for root, dirs, files in os.walk("./"):
     for file in files:
         fileName, fileExtension = os.path.splitext(file)
         if fileExtension in VALID_EXSTENSIONS:
              print ("Path:", root, "File:", file, "Length:", file_length(file))
-
-
-def file_length(file_path):
-    with open(file_path) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
