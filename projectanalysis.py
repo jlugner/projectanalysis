08a1a098e0ba9197b0671b210e564f00875a3f97
Jesper Persson <jespert.persson@gmail.com>
2013-11-07 00:10:45 +0100

    Make sure commented rows are not counted

--- projectanalysis.py
+++ projectanalysis.py
@@ -1,12 +1,19 @@
 import os
-VALID_EXSTENSIONS = [".h", ".hpp", ".c", ".cpp", ".py", ".java", ".rb", ".html", ".css", ".erb", ".m"]
+VALID_EXSTENSIONS  = [".h", ".hpp", ".c", ".cpp", ".py", ".java", ".rb", ".html", ".css", ".erb", ".m"]
+COMMENT_INDICATORS = ["/*", "#", "//", "{-", "--"]
 
 def file_length(file_path):
     with open(file_path) as f:
         rowCount = 0
         for i, l in enumerate(f):
-            if len(l) > 1: rowCount +=1
+            if len(l) > 1 and not is_comment(l[:2]): rowCount +=1
     return rowCount
+
+def is_comment(str):
+    for comment in COMMENT_INDICATORS:
+        if str.startswith(comment):
+            return True
+    return False
 
 # Main program #
 for root, dirs, files in os.walk("./"):
